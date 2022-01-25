ce4e3415b89a7a4328af1a317db88e40ac2ee8e9
Kara Falkowski <kfalkowski11@gmail.com>
2022-01-25 09:20:17 -0600

    add more types

---
+++ src/types/OrderTotals.ts
@@ -0,0 +1,10 @@
+export interface OrderTotals {
+  subTotal: number,
+  shippingTotal: number,
+  excludedTaxes: number,
+  discountTotal: number,
+  total: number,
+  remainingBalance: number,
+  taxesTotal: number,
+  totalItems: number
+}
