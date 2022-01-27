5d1a81b56ca5a08106e11d7af8dfa60aa86d126c
ben.penner <ben.penner@boldcommerce.com>
2022-01-27 08:59:05 -0600

    typing hooks

--- src/hooks/useApplicationState.ts
+++ src/hooks/useApplicationState.ts
@@ -1,9 +1,9 @@
 import { useContext } from 'react';
 import { CheckoutStore } from '../store';
-import { ApplicationState } from '../types/applicationState';
+import { ApplicationState } from '../types/ApplicationState';
 
 const useApplicationState = () : {data: ApplicationState, updateApplicationState: CallableFunction} => {
-  const { state, dispatch } : {state: ApplicationState; dispatch: CallableFunction} = useContext(CheckoutStore);
+  const { state, dispatch } = useContext(CheckoutStore);
 
   const setApplicationStatus = ( newState: ApplicationState ) => {
     dispatch({
